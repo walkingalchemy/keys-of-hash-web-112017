bd407a4f5a15446d98f54c317f721b0d8795d727
James Royer <walkingalchemy@gmail.com>
2017-11-10 20:11:02 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     # code goes here
-    map {|key, value| args.include?(value) ? key : nil }.compact
+    map {|key, value| arguments.include?(value) ? key : nil }.compact
     # binding.pry
   end
 end
